098a20b5a7a14da3366d6e8863ff63030ffedaec
Joe Fletcher <Joe@merchantguru.com>
2014-09-02 16:36:08 -0700

    setting up homepage

--- wp-content/themes/roots/Gruntfile.js
+++ wp-content/themes/roots/Gruntfile.js
@@ -145,6 +145,7 @@ module.exports = function(grunt) {
           'assets/css/main.css',
           'assets/js/scripts.js',
           'templates/*.php',
+          'lib/*.php',
           '*.php'
         ]
       }
