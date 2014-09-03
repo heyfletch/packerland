a27178a6d6fa595e6554488fdc580af5eca36587
Joe Fletcher <Joe@merchantguru.com>
2014-09-03 13:10:50 -0700

    white headline over featured image

--- wp-content/themes/roots/templates/banner.php
+++ wp-content/themes/roots/templates/banner.php
@@ -3,25 +3,34 @@
 	<div class="wrap container" role="document">
 		<div class="content row">
 
-		<main class="main" role="featured">
 
-			<?php
-				$loop = new WP_Query( array(
-				'showposts' => 1,
-				'post__in' => get_option('sticky_posts'),
-				'ignore_sticky_posts' => 1 ) );
 
-			while ( $loop->have_posts() ) : $loop->the_post(); ?>
-				<?php the_post_thumbnail(); ?>
-			
-			<h2><?php the_title(); ?></h2>
-			<?php endwhile; ?>
+		<main class="main featured" role="featured">
+			<article>
+				<header>
 
+					<?php
+						$loop = new WP_Query( array(
+						'showposts' => 1,
+						'post__in' => get_option('sticky_posts'),
+						'ignore_sticky_posts' => 1 ) );
+
+					while ( $loop->have_posts() ) : $loop->the_post(); ?>
+						<?php the_post_thumbnail(); ?>
+					
+					<h2><?php the_title(); ?></h2>
+					<?php endwhile; ?>
+
+				</header>
+			</article>
 		</main>
 
-		<aside class="sidebar" role="trending">
 
-			<h2>Trending: Packer News You Need to Know</h2>
+
+
+		<aside class="sidebar trending" role="trending">
+			<div class="h2">Trending: </div>
+			<h3>Packer News You Need to Know</h3>
 
 			<?php
 				$loop = new WP_Query( array(
@@ -37,7 +46,6 @@
 					<?php } ?>
 
 				<?php endwhile; ?>
-
 		</aside>
 
 
