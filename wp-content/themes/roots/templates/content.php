6d59f80d29339d1bf787748e1601b3c279b9576b
Joe Fletcher <Joe@merchantguru.com>
2014-09-16 22:29:35 -0700

    hyperlink thumbnails on homepage; padding beneath widgets

--- wp-content/themes/roots/templates/content.php
+++ wp-content/themes/roots/templates/content.php
@@ -1,13 +1,13 @@
 <article <?php post_class(); ?>>
 
   <header>
+		<a href="<?php the_permalink(); ?>">
+			<?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
 
-		<?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
+	    <h2 class="entry-title"><?php the_title(); ?></h2>
 
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-    <?php // get_template_part('templates/entry-meta'); ?>
-
+	    <?php // get_template_part('templates/entry-meta'); ?>
+    </a>
   </header>
 
   <div class="entry-summary">
