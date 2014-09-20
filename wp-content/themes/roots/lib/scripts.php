37bfc1b08a3915772afe048c022583b2a78a0692
Joe Fletcher <Joe@merchantguru.com>
2014-09-20 00:02:04 -0700

    revert my try something

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -20,9 +20,7 @@ function roots_scripts() {
    * Read the asset names from assets-manifest.json
    */
   if (WP_ENV === 'development') {
-    $get_assets = file_get_contents(get_template_directory() . '/assets/manifest.json');
-    $assets     = json_decode($get_assets, true);
-    $assets     = array(
+    $assets = array(
       'css'       => '/assets/css/main.css',
       'js'        => '/assets/js/scripts.js',
       'modernizr' => '/assets/vendor/modernizr/modernizr.js',
