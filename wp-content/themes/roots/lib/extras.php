09b7d6e598a7236a18270d3463d5afe526218bcc
Joe Fletcher <Joe@merchantguru.com>
2014-09-22 14:25:28 -0700

    Add Facebook and Google follow links

--- wp-content/themes/roots/lib/extras.php
+++ wp-content/themes/roots/lib/extras.php
@@ -30,6 +30,8 @@ $follow_link_block = <<<FOLLOW
   <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Follow</span></a>
   <ul class="dropdown-menu" role="menu">
     <li class="twitter"><a href="https://twitter.com/packer_land"><span class="glyphicon glyphicon-star"></span> Twitter</a></li>
+    <li class="facebook"><a href="https://www.facebook.com/packer.land.news"><span class="glyphicon glyphicon-star"></span> Facebook</a></li>
+    <li class="google"><a href="https://plus.google.com/+PackerLandNews"><span class="glyphicon glyphicon-star"></span> Google+</a></li>
   </ul>
 </li>
 FOLLOW;
