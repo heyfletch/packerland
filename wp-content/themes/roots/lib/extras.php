ba4b6e6daeaf8ed2b09aaf871fd2caac4d169712
Joe Fletcher <Joe@merchantguru.com>
2014-09-22 14:18:03 -0700

    Social share buttons in navbar; clean up navbar

--- wp-content/themes/roots/lib/extras.php
+++ wp-content/themes/roots/lib/extras.php
@@ -20,3 +20,23 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+// Add stuff to the menu
+add_filter( 'wp_nav_menu_items', 'your_custom_menu_item', 10, 2 );
+function your_custom_menu_item ( $items, $args ) {
+
+$follow_link_block = <<<FOLLOW
+<li class="dropdown">
+  <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Follow</span></a>
+  <ul class="dropdown-menu" role="menu">
+    <li class="twitter"><a href="https://twitter.com/packer_land"><span class="glyphicon glyphicon-star"></span> Twitter</a></li>
+  </ul>
+</li>
+FOLLOW;
+
+
+    if ($args->theme_location == 'primary_navigation') {
+        $items .= $follow_link_block;
+    }
+    return $items;
+}
