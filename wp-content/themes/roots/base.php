1465c98442c24009124d184481071ae4c80f6517
Joe Fletcher <Joe@merchantguru.com>
2014-09-24 20:55:34 -0700

    footer height; clean up columns stuff

--- wp-content/themes/roots/base.php
+++ wp-content/themes/roots/base.php
@@ -19,11 +19,11 @@
 
   <div class="wrap container" role="document">
     <div class="content row">
-      <main class="main" role="main">
+      <main class="col-sm-7" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar" role="complementary">
+        <aside class="col-sm-5" role="complementary">
           <?php include roots_sidebar_path(); ?>
         </aside><!-- /.sidebar -->
       <?php endif; ?>
