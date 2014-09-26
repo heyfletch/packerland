96ad39a7c485786c3eb8b6e2d336af18415f40eb
Joe Fletcher <Joe@merchantguru.com>
2014-09-26 11:25:35 -0700

    add adsense to posts

--- wp-content/themes/roots/templates/content-single.php
+++ wp-content/themes/roots/templates/content-single.php
@@ -14,15 +14,30 @@
   ?>
 
       <?php get_template_part( 'templates/social-share' ); ?>
+
       <h1 class="entry-title"><?php the_title(); ?></h1>
+
       <?php get_template_part('templates/entry-meta'); ?>
+
     </header>
+
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
+
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
+
+    <div class="post-bottom-ad">
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-5674776962249237"
+           data-ad-slot="2624700648"
+           data-ad-format="auto"></ins>
+    </div>
+
     <?php comments_template('/templates/comments.php'); ?>
+
   </article>
 <?php endwhile; ?>
