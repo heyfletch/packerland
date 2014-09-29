776666bb45261667c9344b426cad261b335053b7
Joe Fletcher <Joe@merchantguru.com>
2014-09-28 20:03:03 -0700

    fix tweet link and use wp_get_shortlink

--- wp-content/themes/roots/templates/social-share.php
+++ wp-content/themes/roots/templates/social-share.php
@@ -1,21 +1,21 @@
 <ul class="rrssb-buttons clearfix">
 
   <li class="facebook">
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()) ?>" class="popup">
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(wp_get_shortlink()) ?>" class="popup">
       <?php svg_icon('facebook'); ?>
       <span class="text">share</span>
     </a>
   </li>
 
   <li class="twitter">
-    <a href="http://twitter.com/home?status=<?php echo urlencode(get_the_title()) ?><?php echo urlencode(get_the_permalink()) ?>" class="popup">
+    <a href="http://twitter.com/home?status=<?php echo urlencode(get_the_title()) ?> <?php echo urlencode(wp_get_shortlink()) ?>" class="popup">
       <?php svg_icon('twitter'); ?>
       <span class="text">tweet</span>
     </a>
   </li>
 
   <li class="googleplus">
-    <a href="https://plus.google.com/share?url=<?php echo urlencode(get_the_permalink()) ?>" class="popup">
+    <a href="https://plus.google.com/share?url=<?php echo urlencode(wp_get_shortlink()) ?>" class="popup">
       <?php svg_icon('google_plus'); ?>
       <span class="text">share</span>
     </a>
