827bfc4957dcbd7cdb86efcfa855fc372a3c8f5f
Joe Fletcher <Joe@merchantguru.com>
2014-10-20 11:31:57 -0700

    remove 'us' from Follow buttons

--- wp-content/themes/roots/templates/follow.php
+++ wp-content/themes/roots/templates/follow.php
@@ -6,19 +6,19 @@
 
     <li>
     	<a href="https://twitter.com/packer_land">            
-      	<?php svg_icon('twitter', 'Follow us on Twitter'); ?>
+      	<?php svg_icon('twitter', 'Follow on Twitter'); ?>
       </a>
     </li>
 
     <li>
     	<a href="https://www.facebook.com/packer.land.news">            
-      	<?php svg_icon('facebook', 'Like us on Facebook'); ?>
+      	<?php svg_icon('facebook', 'Like on Facebook'); ?>
       </a>
     </li>
 
     <li>
     	<a href="https://plus.google.com/+PackerLandNews">
-    		<?php svg_icon('google_plus', 'Circle us on Google+'); ?>
+    		<?php svg_icon('google_plus', 'Circle on Google+'); ?>
     	</a>
     </li>
 
