77e56d23d39284c80ff5d7c7ee66698ad2aab983
Joe Fletcher <joe@merchantguru.com>
2016-06-09 11:21:06 -0700

    update migrate plugin

--- wp-content/plugins/wp-migrate-db-pro-cli/version.php
+++ wp-content/plugins/wp-migrate-db-pro-cli/version.php
@@ -1,2 +1,3 @@
 <?php
-$GLOBALS['wpmdb_meta']['wp-migrate-db-pro-cli']['version'] = '1.0.1';
+$GLOBALS['wpmdb_meta']['wp-migrate-db-pro-cli']['version'] = '1.2.4';
+$GLOBALS['wpmdb_meta']['wp-migrate-db-pro-cli']['required-php-version'] = '5.3.2';
